d4c44bb454b1548739fc07c63510230b6bc32cbe
Brijendra-Singh2003 <brijendra0369@gmail.com>
2024-07-01 14:14:23 +0530

    resume link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -52,7 +52,7 @@ export default function Home() {
               <a
                 target="_blank"
                 rel="noreferrer"
-                href="https://drive.google.com/drive/folders/1pSFTRpn2XPitodnZ_FvHEu3e7ZswqXFx?usp=sharing"
+                href="https://drive.google.com/file/d/1cOM6i5WDHGur039nWH5fNuHRASSwZDOu/view?usp=drive_link"
                 className="group relative h-11 w-28 grid place-items-center border-2 border-gray-600 overflow-hidden"
               >
                 <div className="absolute left-0 duration-500 h-full w-0 bg-gray-800 group-hover:w-full" />
